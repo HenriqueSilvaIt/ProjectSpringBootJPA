5ae3959e3ee600209e6fbebd8a85675b69de3d12
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-17 22:27:49 -0300

    Reden

--- src/main/java/com/henriqueproject/course/CourseApplication.java
+++ src/main/java/com/henriqueproject/course/CourseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication // Essas anotation @ é para indicar que é uma aplicação do springboot
 public class CourseApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
